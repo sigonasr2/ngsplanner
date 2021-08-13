921e8564da0564dfbdcf45737a1b8129b8bf216d
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2021-08-13 05:13:20 +0000

    Started skill tree editor work

--- src/skilltree/skillTreeEditor.js
+++ src/skilltree/skillTreeEditor.js
@@ -1,15 +1,20 @@
 import { SkillTree } from "./skillTree";
+import { useState } from "react";
 
 function SkillTreeEditor(p) {
-    return <SkillTree strokeStyle="rgba(0,0,128,1)" lineWidth={3} lineDash={[]}
-                    gridDimensionsX={6} gridDimensionsY={6} gridSizeX={80} gridSizeY={60} gridPaddingX={10} gridPaddingY={10}
-                    skillLines={["□  □  ", //─   □
-                                "└□─┘□□", //│ ├┤┼
-                                " │  ││", //    
-                                " │  □│", //┌ ┐ ┬
-                                " □─□┼□", //└ ┘ ┴
-                                "    □ "]}
-                    />
+    const [lineColor,setLineColor] = useState("#000000")
+
+    return <>
+            <input type="color" value={}/><SkillTree strokeStyle="rgba(0,0,128,1)" lineWidth={3} lineDash={[]}
+                gridDimensionsX={6} gridDimensionsY={6} gridSizeX={80} gridSizeY={60} gridPaddingX={10} gridPaddingY={10}
+                skillLines={["□  □  ", //─   □
+                            "└□─┘□□", //│ ├┤┼
+                            " │  ││", //    
+                            " │  □│", //┌ ┐ ┬
+                            " □─□┼□", //└ ┘ ┴
+                            "    □ "]}
+                />
+            </>
 }
 
 export {SkillTreeEditor}
