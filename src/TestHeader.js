c5a004ea4a9e820566455191deb13d4297ffa01c
dudleycu <dudleyc.twitch@gmail.com>
2021-08-27 06:25:52 +0000

    improved responsive layout stuff, making header look not stupid

--- src/TestHeader.js
+++ src/TestHeader.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 function TestHeader() {
-    return ( 
-        <div className="header">
-        <div>
-        <ul>
-            <li className="logo"><a href="."><img alt="" src={process.env.PUBLIC_URL + '/icons/logo_sm.png'} />&emsp;NGS<span>planner.com</span></a></li>
-            <li className="headermenuitem"><a href=".">Build Planner</a></li>
-            <li className="headermenuitem"><a href=".">Guides</a></li>
-            <li className="headermenuitem"><a href=".">Blog</a></li>
-            <li className="headermenuitem"><a href=".">About</a></li>
-        </ul>
-        </div>
-        </div>
+    return (
+        <header>
+            <div style={{gridArea:"1 / span 3"}}>
+                <ul>
+                    <li className="logo"><a href="."><img alt="" src={process.env.PUBLIC_URL + '/icons/logo_sm.png'} />&emsp;NGS<span>planner.com</span></a></li>
+                    <li className="headerMenuItem"><a href=".">Build Planner</a></li>
+                    <li className="headerMenuItem"><a href=".">Guides</a></li>
+                    <li className="headerMenuItem"><a href=".">Blog</a></li>
+                    <li className="headerMenuItem"><a href=".">About</a></li>
+                </ul>
+            </div>
+        </header>
     )
-    }
+}
 
 export default TestHeader;
