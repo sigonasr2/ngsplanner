3b62b7fa53bc01277724955f8eb7f0e86213ad7f
dudleycu <dudleyc.twitch@gmail.com>
2021-08-29 23:46:02 +0000

    Header

--- src/TestHeader.js
+++ src/TestHeader.js
@@ -4,13 +4,14 @@ function TestHeader() {
     return (
         <header>
             <div style={{gridArea:"1 / span 3"}}>
-                <ul>
-                    <li className="logo"><a href="."><img alt="" src={process.env.PUBLIC_URL + '/icons/logo_sm.png'} />&emsp;NGS<span>planner.com</span></a></li>
+            <div className="logo"><a href="."><img alt="" src={process.env.PUBLIC_URL + '/icons/logo_sm.png'} />&emsp;NGS<span>planner.com</span></a></div>
+               <div className="navigation"> <ul>
                     <li className="headerMenuItem"><a href=".">Build Planner</a></li>
                     <li className="headerMenuItem"><a href=".">Guides</a></li>
                     <li className="headerMenuItem"><a href=".">Blog</a></li>
                     <li className="headerMenuItem"><a href=".">About</a></li>
-                </ul>
+                </ul></div>
+                <div className="navigation2">lol</div>
             </div>
         </header>
     )
