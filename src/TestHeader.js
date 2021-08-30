081f2b3c2e5a08e24eed3993fc409fae1455e3e8
dudleycu <dudleyc.twitch@gmail.com>
2021-08-30 06:17:32 +0000

    Header nearly finished

--- src/TestHeader.js
+++ src/TestHeader.js
@@ -3,7 +3,7 @@ import React from 'react';
 function TestHeader() {
     return (
         <header>
-            <div style={{gridArea:"1 / span 3"}}>
+            <div className="headerWrapper">
             <div className="logo"><a href="."><img alt="" src={process.env.PUBLIC_URL + '/icons/logo_sm.png'} />&emsp;NGS<span>planner.com</span></a></div>
                <div className="navigation"> <ul>
                     <li className="headerMenuItem"><a href=".">Build Planner</a></li>
@@ -11,7 +11,7 @@ function TestHeader() {
                     <li className="headerMenuItem"><a href=".">Blog</a></li>
                     <li className="headerMenuItem"><a href=".">About</a></li>
                 </ul></div>
-                <div className="navigation2">lol</div>
+                <div className="rightNav"><a href="."><img alt="." src={process.env.PUBLIC_URL + '/icons/nicodotpng.png.png'} /> Guest &emsp; <span className="dotMenu">&#xb7;&#xb7;&#xb7;</span></a></div>
             </div>
         </header>
     )
