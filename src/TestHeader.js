1190591a883e9b4a65e327eca66c353954b7b5a5
dudleycu <dudleyc.twitch@gmail.com>
2021-09-02 05:49:51 +0000

    css forever

--- src/TestHeader.js
+++ src/TestHeader.js
@@ -10,7 +10,7 @@ function TestHeader() {
                     <li className="headerMenuItem"><a href=".">Guides</a></li>
                     <li className="headerMenuItem"><a href=".">Blog</a></li>
                     <li className="headerMenuItem"><a href=".">About</a></li></ul>
-                    <section className="miniNav"><a href="."><span className="dotMenu">&#xb7;&#xb7;&#xb7;</span></a></section>
+                    <section className="miniNav"><a href=".">&#9776;</a></section>
                 </div>
                 <div className="rightNav">
                     <section className="loginNav"><a href="."><img alt="." src={process.env.PUBLIC_URL + '/icons/nicodotpng.png.png'} /> Guest &emsp; <span className="dotMenu">&#xb7;&#xb7;&#xb7;</span></a></section>
